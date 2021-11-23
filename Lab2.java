47b4b97e69a6040a6aec450f5a57525749ece903
shoni1 <91070702+shoni1@users.noreply.github.com>
2021-11-23 18:52:36 +0300

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -3,16 +3,16 @@ package Lab2;
 import java.util.Scanner;
 public class Lab2{
     public static void main(String[] args) {
-        int f = 1;
+        int fac = 1;
         System.out.print("Введите число от 0 до 14: ");
         Scanner scanner = new Scanner(System.in);
-        int input_number = scanner.nextInt();
-        if (input_number < 15) {
-            for(int i = 1; i <= input_number; ++i) {
-                f *= i;
+        int num = scanner.nextInt();
+        if (num < 15) {
+            for(int i = 1; i <= num; ++i) {
+                fac *= i;
             }
 
-            System.out.print(input_number + "! = " + f);
+            System.out.print(num + "! = " + fac);
         } else {
             System.out.print("Введите число меньше 15");
         }
